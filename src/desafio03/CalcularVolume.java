e63e3c963c8be5f09356df8f398d9fae815f28ec
Lucas Lenovo <lucas_olivsantos@outlook.com>
2021-04-17 19:23:05 -0300

    Implementando classe de cálculo de volume

--- src/desafio03/CalcularVolume.java
+++ src/desafio03/CalcularVolume.java
@@ -1,5 +1,21 @@
 package desafio03;
 
+import javax.swing.JOptionPane;
 public class CalcularVolume {
+	
+	private double base;
+	private double altura;
+	private double comprimento;
+	private double volume;
+	
+	public void Volume() {
+		base = Double.parseDouble(JOptionPane.showInputDialog("Informe a medida para base (m): "));
+		altura = Double.parseDouble(JOptionPane.showInputDialog("Informe a medida para altura (m): "));
+		comprimento = Double.parseDouble(JOptionPane.showInputDialog("Informe a medida para comprimento (m): "));
+		
+		volume = base * altura * comprimento;
+		
+		JOptionPane.showMessageDialog(null, "O volume calculado: " + volume);
+	}
 	
 }
